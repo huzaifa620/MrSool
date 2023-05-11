a87e78633dfd1cca5b510be8714af5ece9d054a8
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-05-11 20:23:04 +0500

    responsive + data added

--- src/Home.js
+++ src/Home.js
@@ -57,19 +57,19 @@ const Home = () => {
       }, [time]);
 
   return (
-    <div className='flex flex-col items-center w-full h-screen space-y-8 lg:space-y-24 p-2 pt-[10%] lg:pt-[6%]'>
+    <div className='flex flex-col items-center w-full h-screen space-y-8 xl:space-y-24 p-2 pt-[10%] xl:pt-[6%]'>
 
-        <div className='flex flex-col items-center justify-center w-full lg:w-[70%] space-y-4 lg:space-y-8'>
+        <div className='flex flex-col items-center justify-center w-full md:w-[90%] xl:w-[70%] space-y-4 xl:space-y-8'>
             <h2 className='text-3xl md:text-5xl font-bold tracking-widest'> Demand Forecaster </h2>
-            <p className='text-justify lg:text-center text-sm md:text-base'>MRSOOL, a delivery service platform, relies on demand forecasting for various aspects of its operations. By analyzing historical data, market trends, and other relevant factors, MRSOOL can estimate future demand and plan accordingly. This helps in efficient resource planning, including the allocation of delivery drivers, vehicles, and logistics operations. Capacity management is also improved as MRSOOL can adjust its operational capacity to match anticipated demand levels. Additionally, demand forecasting aids in inventory management, ensuring optimal stock levels while minimizing excess inventory. It also assists in optimizing service levels by anticipating peak demand periods and allocating additional resources accordingly. Pricing and promotional strategies can be refined based on demand patterns, leading to increased revenue and resource utilization.</p>
+            <p className='text-justify md:text-center text-sm md:text-base'>MRSOOL, a delivery service platform, relies on demand forecasting for various aspects of its operations. By analyzing historical data, market trends, and other relevant factors, MRSOOL can estimate future demand and plan accordingly. This helps in efficient resource planning, including the allocation of delivery drivers, vehicles, and logistics operations. Capacity management is also improved as MRSOOL can adjust its operational capacity to match anticipated demand levels. Additionally, demand forecasting aids in inventory management, ensuring optimal stock levels while minimizing excess inventory. It also assists in optimizing service levels by anticipating peak demand periods and allocating additional resources accordingly. Pricing and promotional strategies can be refined based on demand patterns, leading to increased revenue and resource utilization.</p>
         </div>
 
-        <div className='flex flex-col lg:flex-row w-full items-center lg:items-start justify-center lg:space-x-12 lg:h-[80%] px-1 lg:px-4 space-y-8 lg:space-y-0'>
+        <div className='flex flex-col xl:flex-row w-full items-center xl:items-start justify-center xl:space-x-12 xl:h-[80%] px-1 xl:px-4 space-y-8 xl:space-y-0'>
 
-            <div className='flex flex-col justify-center w-full lg:w-[65%] h-full space-y-4'>
+            <div className='flex flex-col justify-center w-full xl:w-[65%] h-full space-y-4'>
 
-                <div className='flex flex-col lg:flex-row w-full items-center tracking-widest uppercase font-semibold justify-around lg:space-x-2 space-y-4 lg:space-y-0'>
-                    <p className='w-full lg:w-[70%] uppercase text-2xl md:text-4xl font-semibold text-center lg:text-left'>Delivery Demand Forecaster</p>
+                <div className='flex flex-col xl:flex-row w-full items-center tracking-widest uppercase font-semibold justify-around xl:space-x-2 space-y-4 xl:space-y-0'>
+                    <p className='w-full xl:w-[70%] uppercase text-2xl md:text-4xl font-semibold text-center xl:text-left'>Delivery Demand Forecaster</p>
                     
                     <div className=''>
                         <DatePicker className='border bg-mrsool-green text-white py-2 text-center cursor-pointer rounded-lg px-4 shadow-md focus:outline-none w-[240px] tracking-widest caret-transparent active:bg-white transition-colors duration-500 ease-out' selected={selectedDate} onChange={handleDateChange} />
@@ -87,16 +87,16 @@ const Home = () => {
 
                 </div>
 
-                <div className='lg:text-xl text-center lg:text-left text-red-500'>
+                <div className='xl:text-xl text-center xl:text-left text-red-500'>
                     Blue Circles depicting the restaurants where demand is forcasted with high probability
                 </div>
             </div>
 
-            <div className='flex flex-col lg:justify-center md:items-center w-full lg:w-[35%] lg:h-full space-y-4'>
+            <div className='flex flex-col xl:justify-center md:items-center w-full xl:w-[35%] xl:h-full space-y-4'>
                 <div className='uppercase text-xl md:text-4xl font-semibold text-center'>
                     Rider Demand Forecaster
                 </div>
-                <div className='bg-mrsool-green w-3/4 lg:w-full flex flex-col items-center overflow-y-auto scrollbar-thin scrollbar-track-slate-300 scrollbar-thumb-slate-500 lg:h-[90%] p-2 md:p-4 space-y-2 border-2 border-black rounded-md'>
+                <div className='bg-mrsool-green md:w-3/4 xl:w-full flex flex-col items-center overflow-y-auto scrollbar-thin scrollbar-track-slate-300 scrollbar-thumb-slate-500 xl:h-[90%] p-2 md:p-4 space-y-2 border-2 border-black rounded-md'>
 
                     {
                         data[index]?.restaurants.map((val, ind) => (
@@ -105,7 +105,7 @@ const Home = () => {
                                 <div className='w-[20%] flex items-center justify-center'>
                                     <img src={val.imgUrl} alt='' className='w-16 h-16 scale-75 md:scale-100' />
                                 </div>
-                                <div className='w-[80%] lg:w-full flex flex-col space-y-2 justify-center'>
+                                <div className='w-[80%] xl:w-full flex flex-col space-y-2 justify-center'>
                                     <p className='tracking-widest md:text-xl font-bold'> {val.name} </p>
                                     <div className='flex space-x-3 items-center'>
                                         <img src={car} alt='' className='w-6 h-6' />
@@ -123,8 +123,8 @@ const Home = () => {
 
         </div>
 
-        <div className='flex flex-col w-full lg:w-1/2 h-[80%] space-y-4 pb-24'>
-            <div className='lg:tracking-widest uppercase text-xl md:text-4xl font-semibold text-center lg:text-left'>
+        <div className='flex flex-col w-full xl:w-[55%] h-[80%] space-y-4 pb-24'>
+            <div className='xl:tracking-widest uppercase text-xl md:text-4xl font-semibold text-center xl:text-left'>
                 Optimized Delivery Scheduler
             </div>
             <div className='bg-mrsool-green w-full flex flex-col items-center overflow-y-auto scrollbar-thin scrollbar-track-slate-300 scrollbar-thumb-slate-500 rounded border-2 border-black h-full p-1 lg:p-4 py-8 space-y-4'>
@@ -133,12 +133,12 @@ const Home = () => {
                     data[index]?.restaurants.map((val, ind) => {
                         const isExpanded = expanded[ind] || false;
                         return (
-                            <div key={ind} className='flex border rounded-2xl bg-white lg:space-x-4 p-1 lg:p-4 w-full'>
+                            <div key={ind} className='flex border rounded-2xl bg-white lg:space-x-4 p-1 lg:p-2 xl:p-4 w-full'>
                                 <div className='hidden md:flex w-[20%] md:scale-75 lg:scale-100 items-center'>
                                     <img src={val.imgUrl} alt='' className='w-32 h-32' />
                                 </div>
                                 <div className='w-full flex flex-col space-y-2 justify-center'>
-                                    <p className='tracking-widest text-xl md:text-2xl font-bold text-center lg:text-left'>{val.name}</p>
+                                    <p className='tracking-widest text-xl md:text-3xl font-bold text-center'>{val.name}</p>
                                     <p>Nearby Riders</p>
                                     <div className='flex space-x-3 items-center'>
                                         {isExpanded ? (
@@ -148,10 +148,10 @@ const Home = () => {
                                                         <div className='w-full md:w-[10%]'>
                                                             <img src={imgSrc[value.type]} alt='' className='w-12 h-12' />
                                                         </div>
-                                                        <div className='w-full lg:w-[90%] flex items-center justify-center space-x-1 lg:space-x-4 text-center lg:text-left'>
-                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm'>{value.name}</p>
-                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm'>Average Delivery Time: {value.time} mins</p>
-                                                            <p className='text-xs lg:text-sm'>Expected Earning: {value.earning} SAR</p>
+                                                        <div className='w-full lg:w-[90%] flex items-center justify-center space-x-1 lg:space-x-4 text-center xl:text-left'>
+                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm lg:text-base'>{value.name}</p>
+                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm lg:text-base'>Average Delivery Time: {value.time} mins</p>
+                                                            <p className='text-xs xl:text-sm lg:text-base'>Expected Earning: {value.earning} SAR</p>
                                                         </div>
                                                     </div>
                                                 ))}
@@ -164,10 +164,10 @@ const Home = () => {
                                                         <div className='w-full md:w-[10%]'>
                                                             <img src={imgSrc[value.type]} alt='' className='w-12 h-12' />
                                                         </div>
-                                                        <div className='w-full lg:w-[90%] flex items-center justify-center space-x-1 lg:space-x-4 text-center lg:text-left'>
-                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm'>{value.name}</p>
-                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm'>Average Delivery Time: {value.time} mins</p>
-                                                            <p className='text-xs lg:text-sm'>Expected Earning: {value.earning} SAR</p>
+                                                        <div className='w-full lg:w-[90%] flex items-center justify-center space-x-1 lg:space-x-4 text-center xl:text-left'>
+                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm lg:text-base'>{value.name}</p>
+                                                            <p className='border-r-2 border-slate-500 px-1 lg:px-4 text-xs md:text-sm lg:text-base'>Average Delivery Time: {value.time} mins</p>
+                                                            <p className='text-xs xl:text-sm lg:text-base'>Expected Earning: {value.earning} SAR</p>
                                                         </div>
                                                     </div>
                                                 ))}
--- src/data.js
+++ src/data.js
@@ -187,8 +187,9 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
                 ],
                 imgUrl: 'https://img.freepik.com/premium-vector/fire-wok-asian-food-restaurant-logo_497517-604.jpg?w=2000'
             },
@@ -198,6 +199,9 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 0 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
                 ],
                 imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ8Vc0WrM8mO0xfTjYzxUaDEvq1DPaoc1iag&usqp=CAU'
             },
@@ -207,10 +211,9 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 0 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 }
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
                 ],
                 imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbxoprno9xjr_va3qkqnt5xptcAeYSKF16nle4Sr-IvHO_YdC4c5X26wv56YGQm_76mYE&usqp=CAU" 
             },
@@ -256,12 +259,11 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 0 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 }
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
+                    { name: "Khalid Al-Shehri", time: "25", earning: "40", type: 0 },
+                    { name: "Mansour Al-Mutairi", time: '20', earning: "30", type: 1 }
                 ], 
                 imgUrl: "https://static.spotapps.co/website_images/ab_websites/109590_website/logo.png"
             },
@@ -271,6 +273,11 @@ const data = [
                 ridersDetails:[
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
+                    { name: "Khalid Al-Shehri", time: "25", earning: "40", type: 0 },
+                    { name: "Mansour Al-Mutairi", time: '20', earning: "30", type: 1 }
                 ],
                 imgUrl: "https://www.hyattrestaurants.com/uploaded/restaurant_logos/DMMGH-L006c-stk-TM-color-CMYK-png.png"
             }
@@ -287,8 +294,11 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
+                    { name: "Mansour Al-Mutairi", time: '20', earning: "30", type: 1 },
+                    { name: "Khalid Al-Shehri", time: "25", earning: "40", type: 0 },
                 ],
                 imgUrl: 'https://img.freepik.com/premium-vector/fire-wok-asian-food-restaurant-logo_497517-604.jpg?w=2000'
             },
@@ -298,7 +308,11 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 },
+                    { name: "Khalid Al-Shehri", time: "25", earning: "40", type: 0 },
+                    { name: "Mansour Al-Mutairi", time: '20', earning: "30", type: 1 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
                 ],
                 imgUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZ8Vc0WrM8mO0xfTjYzxUaDEvq1DPaoc1iag&usqp=CAU'
             },
@@ -307,7 +321,11 @@ const data = [
                 riders: 12, 
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 0 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 }
+                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
+                    { name: "Khalid Al-Shehri", time: "25", earning: "40", type: 0 },
                 ],
                 imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbxoprno9xjr_va3qkqnt5xptcAeYSKF16nle4Sr-IvHO_YdC4c5X26wv56YGQm_76mYE&usqp=CAU" 
             },
@@ -317,9 +335,10 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 0 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
+                    { name: "Mansour Al-Mutairi", time: '20', earning: "30", type: 1 }
                 ], 
                 imgUrl: "https://i.pinimg.com/originals/fa/52/4d/fa524dff3190583560e5730c506ad6a8.jpg"
             },
@@ -329,11 +348,11 @@ const data = [
                 ridersDetails: [
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 0 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
-                    { name: "Hashim Sarwar", time: "25", earning: "40", type: 0 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 }
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
+                    { name: "Mansour Al-Mutairi", time: '20', earning: "30", type: 1 },
+                    { name: "Khalid Al-Shehri", time: "25", earning: "40", type: 0 },
                 ], 
                 imgUrl: "https://static.spotapps.co/website_images/ab_websites/109590_website/logo.png"
             },
@@ -343,7 +362,11 @@ const data = [
                 ridersDetails:[
                     { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
                     { name: "Hashim Sarwar", time: "25", earning: "40", type: 1 },
-                    { name: "Muhammad Baqar", time: '20', earning: "30", type: 1 },
+                    { name: "Mansour Al-Mutairi", time: '20', earning: "30", type: 1 },
+                    { name: "Khalid Al-Shehri", time: "25", earning: "40", type: 0 },
+                    { name: "Abdullah Al-Saud", time: '20', earning: "30", type: 0 },
+                    { name: "Nasser Al-Fahad", time: "25", earning: "40", type: 1 },
+                    { name: "Saad Al-Omari", time: '20', earning: "30", type: 1 },
                 ],
                 imgUrl: "https://www.hyattrestaurants.com/uploaded/restaurant_logos/DMMGH-L006c-stk-TM-color-CMYK-png.png"
             },
