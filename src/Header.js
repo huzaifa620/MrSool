419fad27de8bc98817cd1a1ebb6a3e73c63e359c
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-05-11 19:56:47 +0500

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex items-center justify-between w-full bg-black p-3 text-white text-lg font-sans shadow-2xl fixed top-0 z-20'>
-        <div className='cursor-pointer w-1/3 flex items-center justify-center'>
+    <div className='flex items-center justify-between w-full bg-black lg:px-3 py-3 text-white text-lg font-sans shadow-2xl fixed top-0 z-20'>
+        <div className='cursor-pointer w-1/3 flex items-center justify-center scale-75 lg:scale-100'>
             <img src='https://mrsool.co/images/Mrsool-Logo-v2.png' alt='' />
         </div>
         <div className='cursor-pointer w-1/3 hidden md:flex items-center justify-center'>
-            <p className='text-2xl underline decoration-mrsool-green underline-offset-8 decoration-4'> Demand Forecaster Dashboard </p>
+            <p className='text-lg lg:text-2xl underline decoration-mrsool-green underline-offset-8 decoration-4'> Demand Forecaster Dashboard </p>
         </div>
         <div className='w-1/3 flex items-center justify-center'>
             <div className='bg-[#E10600] hover:bg-[#af0500] text-center items-center justify-center rounded-3xl px-4 py-1 cursor-pointer text-xs md:text-base'>
