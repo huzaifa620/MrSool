e9d90b6182dfd9754930dc121850e8049a75601f
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-05-16 20:30:58 +0500

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useContext } from 'react'
 import { Context } from './Context';
 
 const Form = () => {
@@ -9,24 +9,24 @@ const Form = () => {
 
     <div className='w-[40%] absolute top-4 left-4'>
         <div className="flex">
-            <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+            <label htmlFor="search-dropdown" className="mb-2 text-sm font-medium text-gray-900 sr-only"></label>
 
             <button id="dropdown-button" data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:outline-none active:bg-mrsool-green transition-colors duration-500 ease-out" type="button"> {country} <svg aria-hidden="true" className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
 
-            <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 w-1/4">
+            <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-1/4">
 
                 <ul className="py-2 text-sm text-black flex flex-col items-center justify-center w-full" aria-labelledby="dropdown-button">
                     <li className='w-full'>
-                        <button type="button" onClick={() => {setCountry('KSA')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Saudi Arabia</button>
+                        <button onClick={() => {setCountry('KSA')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100">Saudi Arabia</button>
                     </li>
                     <li className='w-full'>
-                        <button type="button" onClick={() => {setCountry('UAE')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">UAE</button>
+                        <button onClick={() => {setCountry('UAE')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100">UAE</button>
                     </li>
                     <li className='w-full'>
-                        <button type="button" onClick={() => {setCountry('QAT')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Qatar</button>
+                        <button onClick={() => {setCountry('QAT')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100">Qatar</button>
                     </li>
                     <li className='w-full'>
-                        <button type="button" onClick={() => {setCountry('EGY')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Egypt</button>
+                        <button onClick={() => {setCountry('EGY')}} className="inline-flex w-full items-center justify-center px-4 py-2 hover:bg-gray-100">Egypt</button>
                     </li>
                 </ul>
             </div>
