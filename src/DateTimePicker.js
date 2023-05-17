8f921544ea4327e8fdd3d2d779a55d3bbe3e2640
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-05-17 20:29:15 +0500

    Update DateTimePicker.js

--- src/DateTimePicker.js
+++ src/DateTimePicker.js
@@ -3,25 +3,26 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDateTimePicker } from '@mui/x-date-pickers/MobileDateTimePicker';
+import { useMediaQuery } from '@mui/material';
 import { Context } from './Context';
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 
 export default function DateTimePicker() {
-
-  const {time, setTime} = useContext(Context)
+  const { time, setTime } = useContext(Context);
 
   const handleTimeChange = (newTime) => {
-    console.log('Selected time:', newTime.$H);
-    setTime(newTime.$H)
+    setTime(newTime.$H);
   };
+
+  const orientation = window.innerWidth < 480 ? 'portrait' : 'landscape';
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <DemoContainer components={['DateTimePicker']}>
         <MobileDateTimePicker
-        defaultValue={dayjs()}
-        orientation="landscape"
-        onChange={handleTimeChange}
+          defaultValue={dayjs()}
+          orientation={orientation}
+          onChange={handleTimeChange}
         />
       </DemoContainer>
     </LocalizationProvider>
