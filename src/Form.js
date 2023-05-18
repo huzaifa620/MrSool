a252290aabe6eccac81f4e25e285bf32451db525
Muhammad Huzaifa <79418234+huzaifa620@users.noreply.github.com>
2023-05-19 00:25:52 +0500

    Update Form.js

--- src/Form.js
+++ src/Form.js
@@ -72,7 +72,7 @@ const Form = () => {
 
             <button id="dropdown-button" data-dropdown-toggle="dropdown" className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-1 md:px-4 text-xs md:text-sm md:font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:outline-none active:bg-mrsool-green transition-colors duration-500 ease-out" type="button"> {country} <svg aria-hidden="true" className="w-4 h-4 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg></button>
 
-            <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow md:w-1/4">
+            <div id="dropdown" className="z-20 hidden bg-white divide-y divide-gray-100 rounded-lg shadow md:w-1/4">
 
                 <ul className="py-2 z-20 text-xs md:text-sm text-black flex flex-col items-center justify-center w-full" aria-labelledby="dropdown-button">
                     <li className='w-full'>
